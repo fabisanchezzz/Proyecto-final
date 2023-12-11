5c6377b777ee7da8513afa612a43a4b7dfa8ca23
Leandro Rúa <j.leandrorua@gmail.com>
2023-12-11 15:43:40 -0600

    add imports

--- API/project/src/main/java/com/project/project/PRestController.java
+++ API/project/src/main/java/com/project/project/PRestController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
